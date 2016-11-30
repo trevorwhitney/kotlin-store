55072bb6e8a9ee90695e2570bcf08a4e1c5419b4
Trevor Whitney <twhitney@pivotal.io>
2016-11-30 11:14:43 -0700

    initial commit

---
+++ src/main/kotlin/com/trevorwhitney/kotlinstore/KotlinStoreApplication.kt
@@ -0,0 +1,9 @@
+@SpringBootApplication
+class KotlinStoreApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(KotlinStoreApplication.class, args)
+        }
+    }
+}
